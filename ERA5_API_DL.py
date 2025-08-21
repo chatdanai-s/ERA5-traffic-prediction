14e696d88f6afed215e99560aee1ade88e3a8870
Chatdanai Sawangwong <chatdanai.saw@gmail.com>
2025-08-21 08:53:25 +0700

    Added files

---
+++ ERA5_API_DL.py
@@ -0,0 +1,54 @@
+import cdsapi
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": ["reanalysis"],
+    "variable": [
+        "2m_temperature",
+        "total_precipitation",
+        "high_cloud_cover",
+        "low_cloud_cover",
+        "medium_cloud_cover",
+        "total_cloud_cover",
+        "convective_precipitation",
+        "large_scale_precipitation",
+        "precipitation_type",
+        "snowfall"
+    ],
+    "year": ["2012"],
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "grib",
+    "download_format": "zip",
+    "area": [44.96, -93.14, 44.95, -93.13]
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
